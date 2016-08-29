dc03c92e8c30d2bdc91fc4477ae5b5c410dc9d2f
username <richdev135@gmail.com>
2016-08-29 16:14:07 -0600

    Code for Step-2

--- content.js
+++ content.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
   $("#testbtn").on("click", function(){
     $("#heading").html("After click");
+
+    chrome.tabs.executeScript(null,{file:"scripts.js"});
   });
 });
 
